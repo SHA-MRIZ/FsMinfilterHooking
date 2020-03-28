39249f5a102dfd4dad98a71582bd8d533012fafe
unknown <aviadshamriz@gmail.com>
2020-03-28 20:00:57 +0300

    Hooking by getting offset by dummy filter

--- FsMinifilterHooking/FsMinifilterHooking.cpp
+++ FsMinifilterHooking/FsMinifilterHooking.cpp
@@ -1,136 +1,253 @@
 #include "FsMinifilterHooking.h"
 
-FLT_PREOP_CALLBACK_STATUS HookPreOperationFunction(PFLT_CALLBACK_DATA Data,
-                                                   PCFLT_RELATED_OBJECTS FltObjects,
-                                                   PVOID* CompletionContext)
+
+PCALLBACK_NODE getCachedCallbackNodesByInstance(PFLT_INSTANCE instance)
+{
+    for (size_t instanceIndex = 0; instanceIndex < originalInstances->getSize(); instanceIndex++)
+    {
+        if (instance == (*originalInstances)[instanceIndex]->get())
+        {
+            return (*originalInstances)[instanceIndex]->getCachedCallbackNodes();
+        }
+    }
+
+    return nullptr;
+}
+
+bool isCallbackNode(
+    PCALLBACK_NODE potentialCallbackNode,
+    PFLT_INSTANCE pFltInstance,
+    void* preCallback)
+{
+    return ((potentialCallbackNode->PreOperation == preCallback) &&
+            (potentialCallbackNode->Instance == pFltInstance));
+}
+
+unsigned short getOffsetOfCallbackNodes(
+    FltFilterGuard& filter,
+    unsigned short limit,
+    void * preCallbackFunc,
+    unsigned short callbackIndex)
+{
+    void* potentialPointer;
+    unsigned short offset = FltInstanceGuard::INVALID_OFFSET;
+    ArrayGuard<FltInstanceGuard*, true> instancesArray;
+    unsigned long fltInstancesNumber = filter.getInstances(instancesArray);
+
+    if (fltInstancesNumber == 0)
+    {
+        return offset;
+    }
+
+    FltVolumeGuard volume(*instancesArray[0]);
+
+    for (unsigned short i = 0; i < limit / sizeof(unsigned short); i++)
+    {
+        potentialPointer = reinterpret_cast<void*>(*(reinterpret_cast<PULONG_PTR>(
+            reinterpret_cast<unsigned short*>(instancesArray[0]->get()) + i)));
+
+        if (MmIsAddressValid(potentialPointer) &&
+            (potentialPointer != filter.get()) &&
+            (potentialPointer != volume.get()) &&
+            (isCallbackNode(
+                reinterpret_cast<PCALLBACK_NODE>(potentialPointer),
+                instancesArray[0]->get(),
+                preCallbackFunc)))
+        {
+            offset = i * sizeof(unsigned short) - callbackIndex * sizeof(PCALLBACK_NODE);
+            break;
+        }
+    }
+
+    return offset;
+}
+
+FLT_PREOP_CALLBACK_STATUS dummyCreatePreOperation(PFLT_CALLBACK_DATA Data,
+    PCFLT_RELATED_OBJECTS FltObjects,
+    PVOID* CompletionContext)
 {
     UNREFERENCED_PARAMETER(Data);
     UNREFERENCED_PARAMETER(FltObjects);
     UNREFERENCED_PARAMETER(CompletionContext);
-    return FLT_PREOP_SUCCESS_WITH_CALLBACK;
+
+    return FLT_PREOP_SUCCESS_NO_CALLBACK;
 }
 
-FLT_POSTOP_CALLBACK_STATUS HookPostOperationFunction(PFLT_CALLBACK_DATA Data,
-                                                     PCFLT_RELATED_OBJECTS FltObjects,
-                                                     PVOID CompletionContext,
-                                                     FLT_POST_OPERATION_FLAGS Flags)
+FLT_PREOP_CALLBACK_STATUS hookPreOperationFunction(
+    PFLT_CALLBACK_DATA Data,
+    PCFLT_RELATED_OBJECTS FltObjects,
+    PVOID* CompletionContext)
 {
     UNREFERENCED_PARAMETER(Data);
-    UNREFERENCED_PARAMETER(FltObjects);
+    UNREFERENCED_PARAMETER(CompletionContext);
+
+    PCALLBACK_NODE originalCallbackNodes = getCachedCallbackNodesByInstance(FltObjects->Instance);
+
+    if (originalCallbackNodes == nullptr)
+    {
+        return FLT_PREOP_SUCCESS_NO_CALLBACK;
+    }
+
+    auto originalPreCallback = reinterpret_cast<PreCallbackProt>(
+        originalCallbackNodes[static_cast<INT8>(Data->Iopb->MajorFunction) + 0x16].PreOperation);
+
+    return originalPreCallback(Data, FltObjects, CompletionContext);
+}
+
+FLT_POSTOP_CALLBACK_STATUS hookPostOperationFunction(
+    PFLT_CALLBACK_DATA Data,
+    PCFLT_RELATED_OBJECTS FltObjects,
+    PVOID CompletionContext,
+    FLT_POST_OPERATION_FLAGS Flags)
+{
     UNREFERENCED_PARAMETER(CompletionContext);
     UNREFERENCED_PARAMETER(Flags);
-    return FLT_POSTOP_FINISHED_PROCESSING;
+
+    PCALLBACK_NODE originalCallbackNodes = getCachedCallbackNodesByInstance(FltObjects->Instance);
+
+    if (originalCallbackNodes == nullptr)
+    {
+        return FLT_POSTOP_FINISHED_PROCESSING;
+    }
+
+    auto originalPostCallback = reinterpret_cast<PostCallbackProt>(
+        originalCallbackNodes[static_cast<INT8>(Data->Iopb->MajorFunction) + 0x16].PostOperation);
+
+    return originalPostCallback(Data, FltObjects, CompletionContext, Flags);
 }
 
-VOID HookCallbacks(PFLT_FILTER pFilter)
+void hookFunction(PVOID* pSourceFunction, PVOID hookFunction)
 {
-    NTSTATUS status;
-    PFLT_INSTANCE* instancesList = nullptr;
-    ULONG instancesListSize = 0;
-    ULONG instancesNumber = 0;
-    status = FltEnumerateInstances(NULL, pFilter, NULL, 0, &instancesNumber);
-
-    if ((status == STATUS_BUFFER_TOO_SMALL) && instancesNumber)
+    if (MmIsAddressValid(*pSourceFunction))
     {
-        instancesListSize = sizeof(PFLT_INSTANCE) * instancesNumber * 2;
-        instancesList = (PFLT_INSTANCE*)ExAllocatePool(NonPagedPool, instancesListSize);
-        if (instancesList)
+        InterlockedExchangePointer(pSourceFunction, hookFunction);
+    }
+}
+
+VOID HookCallbacks(FltFilterGuard& filter)
+{
+    ArrayGuard<FltInstanceGuard*, true> instancesArray;
+    ULONG instancesCount = filter.getInstances(instancesArray);
+
+    if (instancesCount == 0)
+    {
+        return;
+    }
+
+    originalInstances = new ArrayGuard<FltInstanceGuard*, true>();
+    originalInstances->allocate(NonPagedPool, instancesCount);
+
+    for (size_t instanceIndex = 0; instanceIndex < instancesCount; instanceIndex++)
+    {
+        (*originalInstances)[instanceIndex] =
+            new FltInstanceGuard(instancesArray[instanceIndex]->get(), false, false, true);
+        (*originalInstances)[instanceIndex]->cachingCallbackNodes();
+
+        PCALLBACK_NODE* callbackNodesList =
+            (*originalInstances)[instanceIndex]->getPointerToCallbackNodesField();
+
+        for (size_t callbackIndex = 0X16;
+             callbackIndex <= IRP_MJ_MAXIMUM_FUNCTION + 0X16;
+             callbackIndex++)
         {
-            status = FltEnumerateInstances(0, pFilter, instancesList, instancesListSize, &instancesNumber);
+            PCALLBACK_NODE callbackNode = callbackNodesList[callbackIndex];
 
-            for (ULONG i = 0; i < instancesNumber; i++)
+            if (MmIsAddressValid(callbackNode))
             {
-                DbgPrint("PFLT_FILTER: %x, Instances: %x \n", pFilter, instancesList[i]);
-
-                for (INT j = 0; j < 50; j++)
-                {
-                    PCALLBACK_NODE pCallbackNode = (PCALLBACK_NODE)(*((ULONG_PTR*)((ULONG_PTR)instancesList[i] + OffsetCallbackNode + 8 * j)));
-
-                    if ((pCallbackNode != NULL) && (MmIsAddressValid(pCallbackNode)))
-                    {
-                        if (MmIsAddressValid(pCallbackNode->PreOperation))
-                        {
-                            InterlockedExchange((PLONG)(&(pCallbackNode->PreOperation)), (LONG)(((ULONG_PTR)(&HookPreOperationFunction))));
-                        }
-
-                        if (MmIsAddressValid(pCallbackNode->PostOperation))
-                        {
-                            InterlockedExchange((PLONG)(&(pCallbackNode->PostOperation)), (LONG)(((ULONG_PTR)(&HookPostOperationFunction))));
-                        }
-
-                    }
-                }
-                FltObjectDereference(instancesList[i]);
+                hookFunction(&(callbackNode->PreOperation), &hookPreOperationFunction);
+                hookFunction(&(callbackNode->PostOperation), &hookPostOperationFunction);
             }
         }
     }
 }
 
+VOID UnHookCallbacks()
+{
+    if (originalInstances == nullptr)
+    {
+        return;
+    }
+
+    for (size_t instanceIndex = 0;
+         instanceIndex < originalInstances->getSize();
+         instanceIndex++)
+    {
+        FltInstanceGuard instance((*originalInstances)[instanceIndex]->get(), true, true, false);
+
+        if (!instance.isValid())
+        {
+            // The instance is tear down
+            continue;
+        }
+
+        PCALLBACK_NODE* callbackNodesList = instance.getPointerToCallbackNodesField();
+        auto cachedCallbackNodes = (*originalInstances)[instanceIndex]->getCachedCallbackNodes();
+
+        for (size_t callbackIndex = 0x16;
+             callbackIndex <= IRP_MJ_MAXIMUM_FUNCTION + 0x16;
+             callbackIndex++)
+        {
+            PCALLBACK_NODE callbackNode = callbackNodesList[callbackIndex];
+
+            if (MmIsAddressValid(callbackNode))
+            {
+                hookFunction(
+                    &(callbackNode->PreOperation),
+                    cachedCallbackNodes[callbackIndex].PreOperation);
+
+                hookFunction(
+                    &(callbackNode->PostOperation),
+                    cachedCallbackNodes[callbackIndex].PostOperation);
+            }
+
+        }
+    }
+
+    delete originalInstances;
+}
+
 VOID Unload(PDRIVER_OBJECT  DriverObject)
 {
-
-    UNICODE_STRING usDosDeviceName;
-
-/*    UNICODE_STRING name;
-    RtlInitUnicodeString(&name, L"PROCMON23");
-    PFLT_FILTER flt = Get_Filter_Object(&name);
-    DbgPrint("PreCallbackSaved: 0x%x", preCallbackSaved)*/;
-    //HookCallbacks(flt, (PVOID)preCallbackSaved, (PVOID)postCallbackSaved, DriverObject);
+    UNREFERENCED_PARAMETER(DriverObject);
 
     DbgPrint("Unload Driver \r\n");
 
-    RtlInitUnicodeString(&usDosDeviceName, L"\\DosDevices\\MinifilterHooking");
-    IoDeleteSymbolicLink(&usDosDeviceName);
-
-    IoDeleteDevice(DriverObject->DeviceObject);
+    UnHookCallbacks();
 }
 
 extern "C" NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
 {
-    NTSTATUS status;
-
     UNREFERENCED_PARAMETER(RegistryPath);
 
-    PDEVICE_OBJECT pDeviceObject = NULL;
-    UNICODE_STRING usDriverName, usDosDeviceName;
+    NTSTATUS status = STATUS_SUCCESS;
 
     DbgPrint("DriverEntry Called \r\n");
 
-    RtlInitUnicodeString(&usDriverName, L"\\Device\\MinifilterHooking");
+    DriverObject->DriverUnload = Unload;
 
-    status = IoCreateDevice(DriverObject, 0, &usDriverName, FILE_DEVICE_UNKNOWN, FILE_DEVICE_SECURE_OPEN, FALSE, &pDeviceObject);
+    FltFilterGuard dummyFilter(DriverObject, FilterRegistration);
+    dummyFilter.startFiltering();
 
-    if (NT_SUCCESS(status))
+    unsigned short offsetCallbackNodes = getOffsetOfCallbackNodes(
+        dummyFilter,
+        0x230,
+        dummyCreatePreOperation,
+        IRP_MJ_CREATE + 0X16);
+
+    dummyFilter.~FltFilterGuard();
+
+    if (offsetCallbackNodes != FltInstanceGuard::INVALID_OFFSET)
     {
-        pDeviceObject->Flags &= (~DO_DEVICE_INITIALIZING);
+        FltInstanceGuard::setOffsetCallbackNodes(offsetCallbackNodes);
 
-        RtlInitUnicodeString(&usDosDeviceName, L"\\DosDevices\\MinifilterHooking");
+        const UNICODE_STRING filterName = RTL_CONSTANT_STRING(L"luafv");
 
-        status = IoCreateSymbolicLink(&usDosDeviceName, &usDriverName);
-        if (NT_SUCCESS(status))
+        FltFilterGuard filter(&filterName);
+
+        if (filter.isValid())
         {
-            DbgPrint("IoCreateSymbolicLink success");
+            HookCallbacks(filter);
         }
-        else
-        {
-            DbgPrint("IoCreateSymbolicLink failed: error - %d", status);
-        }
-
-        DriverObject->DriverUnload = Unload;
-
-        UNICODE_STRING name;
-        RtlInitUnicodeString(&name, L"PROCMON24");
-        //RtlInitUnicodeString(&name, L"luafv");
-        PFLT_FILTER flt;
-        if (NT_SUCCESS(FltGetFilterFromName(&name, &flt)))
-        {
-            HookCallbacks(flt);
-        }
-        //FltpGetCallbackNodeForInstance((PFLT_INSTANCE)0x02, 5);
-    }
-    else
-    {
-        DbgPrint("IoCreateDevice Failed: error - %d", status);
     }
 
     return status;
--- FsMinifilterHooking/FsMinifilterHooking.h
+++ FsMinifilterHooking/FsMinifilterHooking.h
@@ -1,16 +1,56 @@
 #pragma once
-//#pragma comment(lib, "fltMgr.lib")
 
-//#include "fltMgr.h"
 #include <fltKernel.h>
+#include "PrivateStructs.h"
+#include "RAIIUtils.h"
 
-ULONG OffsetCallbackNode = 0xa0;
-ULONG OffsetPreOperation = 0x18;
-ULONG OffsetPostOperation = 0x20;
-ULONG offsetName = 0x38;
-const ULONG POOL_TAG = 5070;
 
-extern "C" DRIVER_INITIALIZE DriverEntry; 
+typedef FLT_PREOP_CALLBACK_STATUS(*PreCallbackProt)(
+    _Inout_ PFLT_CALLBACK_DATA Data,
+    _In_ PCFLT_RELATED_OBJECTS FltObjects,
+    _Flt_CompletionContext_Outptr_ PVOID* CompletionContext);
+
+typedef FLT_POSTOP_CALLBACK_STATUS(*PostCallbackProt)(
+    _Inout_ PFLT_CALLBACK_DATA Data,
+    _In_ PCFLT_RELATED_OBJECTS FltObjects,
+    _In_opt_ PVOID CompletionContext,
+    _In_ FLT_POST_OPERATION_FLAGS Flags);
+
+// array that contain all instances that hooked and their original CallbackNodes
+ArrayGuard<FltInstanceGuard*, true>* originalInstances = nullptr;
+
+FLT_PREOP_CALLBACK_STATUS dummyCreatePreOperation(
+    PFLT_CALLBACK_DATA Data,
+    PCFLT_RELATED_OBJECTS FltObjects,
+    PVOID* CompletionContext);
+
+CONST FLT_OPERATION_REGISTRATION Callbacks[] = {
+        { IRP_MJ_CREATE,
+          0,
+          dummyCreatePreOperation,
+          NULL },
+
+    { IRP_MJ_OPERATION_END }
+};
+
+const FLT_REGISTRATION FilterRegistration = {
+    sizeof(FLT_REGISTRATION),           //  Size
+    FLT_REGISTRATION_VERSION,           //  Version
+    0,                                  //  Flags
+    NULL,                               //  Context Registration.
+    Callbacks,                          //  Operation callbacks
+    NULL,                               //  FilterUnload
+    NULL,                               //  InstanceSetup
+    NULL,                               //  InstanceQueryTeardown
+    NULL,                               //  InstanceTeardownStart
+    NULL,                               //  InstanceTeardownComplete
+    NULL,                               // GenerateFileName
+    NULL,                               // NormalizeNameComponent
+    NULL,                               //  NormalizeContextCleanup
+};
+
+extern "C" DRIVER_INITIALIZE DriverEntry;
+
 extern "C" NTSTATUS
 DriverEntry(
     _In_ PDRIVER_OBJECT DriverObject,
@@ -19,40 +59,5 @@ DriverEntry(
 
 #pragma alloc_text(INIT, DriverEntry)
 
-enum CALLBACK_NODE_FLAGS
-{
-    CBNFL_SKIP_PAGING_IO = 1,
-    CBNFL_SKIP_CACHED_IO = 2,
-    CBNFL_USE_NAME_CALLBACK_EX = 4,
-    CBNFL_SKIP_NON_DASD_IO = 8,
-};
-
-typedef struct _CALLBACK_NODE
-{
-    LIST_ENTRY CallbackLinks;
-    PFLT_INSTANCE Instance;
-    PVOID PreOperation;
-    PVOID PostOperation;
-    LONG GenerateFileName;
-    LONG NormalizeNameComponent;
-    LONG NormalizeNameComponentEx;
-    PVOID NormalizeContextCleanup;
-    int Flags;
-} CALLBACK_NODE, *PCALLBACK_NODE;
-
-typedef FLT_PREOP_CALLBACK_STATUS
-PreCallbackProt(
-    _Inout_ PFLT_CALLBACK_DATA Data,
-    _In_ PCFLT_RELATED_OBJECTS FltObjects,
-    _Flt_CompletionContext_Outptr_ PVOID* CompletionContext
-);
-
-typedef FLT_POSTOP_CALLBACK_STATUS
-PostCallbackProt(
-    _Inout_ PFLT_CALLBACK_DATA Data,
-    _In_ PCFLT_RELATED_OBJECTS FltObjects,
-    _In_opt_ PVOID CompletionContext,
-    _In_ FLT_POST_OPERATION_FLAGS Flags
-);
-
-//extern "C" PCallbackNode FltpGetCallbackNodeForInstance(PFLT_INSTANCE fltInstance, unsigned int a2);
+// Get CallbackNodes from global instances array by specific instance in parameter
+PCALLBACK_NODE getCachedCallbackNodesByInstance(PFLT_INSTANCE instance);
